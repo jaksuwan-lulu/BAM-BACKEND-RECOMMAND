ca453dbb2784b10ce8739fe02390dfc1d31101c3
jaksuwan-lulu <jaksuwan.wong@gmail.com>
2024-11-28 22:38:04 +0700

    main.py updates

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 # รวม router ต่างๆ พร้อมกำหนดการบังคับใช้ token ใน endpoint ที่ต้องการการรับรองความปลอดภัย
 app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 app.include_router(user.router, prefix="/users", tags=["Users"], dependencies=[Depends(oauth2_scheme)])
-app.include_router(house.router, prefix="/house", tags=["Houses and Favorites"])  # ไม่ต้องใช้ Depends สำหรับการเข้าถึงทั่วไป
+app.include_router(house.router, prefix="/house", tags=["Houses"])  # ไม่ต้องใช้ Depends สำหรับการเข้าถึงทั่วไป
 app.include_router(recommendation.router, prefix="/recommendation", tags=["Recommendations"], dependencies=[Depends(oauth2_scheme)])
 # app.include_router(visited_pages.router, prefix="/visited_page", tags=["Visited Pages"], dependencies=[Depends(oauth2_scheme)])
 app.add_exception_handler(HTTPException, http_error_handler)
